3a2997cb2002a96893977ec41df1d2c0a2f9da51
Bremen <vance_br@mercer.edu>
2023-07-26 10:54:02 -0400

    Setup app with Django
    Created a structure for the website, including the template and static directories.

---
+++ Macon_Code/Macon_Code/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for Macon_Code project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Macon_Code.settings')
+
+application = get_wsgi_application()
