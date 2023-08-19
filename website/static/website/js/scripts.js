416abb34382bef1628367e7fbc7d3274ddb11786
Christopher Josey <ctjosey@gmail.com>
2023-08-19 16:10:07 -0400

    Fix carousel buttons (#4)

---
+++ website/static/website/js/scripts.js
@@ -0,0 +1,11 @@
+//DOMContentLoaded ensures that anything in the function occurs after the html elements
+//are loaded
+document.addEventListener('DOMContentLoaded', function() {
+
+    // Just a generic function for future reference
+    function handleIndicatorClick(param) {
+        // Your code to execute when the button is clicked
+        console.log(param);
+    }
+
+});
