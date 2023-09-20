41db89151ed1da6efed8d5e4684fae0244fade53
jeijames <44823540+carrot10pumpkin@users.noreply.github.com>
2023-09-20 18:37:12 -0400

    Overhaul
    Fixed code

---
+++ MaconCoders/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path 
+from django.contrib import admin
+from django.urls import include, path
+
+from . import views
+urlpatterns = [
+       path('admin/',admin.site.urls),
+    path('', views.index, name='index'),
+    
+   
+ 
+
+]
