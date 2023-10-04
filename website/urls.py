8d1cfc6ce3d52496e30195584c9afa01ebd72e77
Christopher Josey <ctjosey@gmail.com>
2023-10-04 17:42:17 -0400

    Fake project (#18)
    Add placeholder templates for other site pages.
    
    * Adding short commit to save my new token
    * Adjusted earlier changes with the new and improved page
    * Added functionality to the other buttons
    * Edited some text

--- website/urls.py
+++ website/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("projects/", views.projects, name="projects"),
+    path("suggestionForm/", views.suggestionForm, name="suggestionForm"),
+    path("aboutUs/", views.aboutUs, name="aboutUs"),
 ]
--- website/views.py
+++ website/views.py
@@ -2,3 +2,12 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "website/index.html")
+
+def projects(request):
+    return render(request, "website/projects.html")
+
+def suggestionForm(request):
+    return render(request, "website/suggestionForm.html")
+
+def aboutUs(request):
+    return render(request, "website/aboutUs.html")
